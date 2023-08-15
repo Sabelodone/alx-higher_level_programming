449bb97efb20d050be8b3b1e4973c6b4ff0730d9
Sabelodone <sabelozondo825gmail.com>
2023-08-15 03:59:52 -0700

    Write a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    
     a = 1
     b = 2
+    from add_0 import add
+
     result = add(a, b)
     print(f"{a} + {b} = {result}")
