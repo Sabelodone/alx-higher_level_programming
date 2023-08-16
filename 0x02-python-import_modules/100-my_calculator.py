ca6d52767841126011f5331e2cfebce8476f52b5
Sabelodone <sabelozondo825gmail.com>
2023-08-16 04:56:10 -0700

    Write a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,16 +2,22 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if len(sys.argv) - 1 != 3:
+if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit (1)
+    sys.exit(1)
 
-    ops = {"+" : add, "-" : sub, "*" : mul, "/" : div}
+ops = {"+": add, "-": sub, "*": mul, "/": div}
 
-    if sys.argv[2] not in list(ops.keys()):
-         print("Unknown operator. Available operators: +, -, * and /")
-         sys.exit(1)
+if sys.argv[2] not in ops:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+operator = sys.argv[2]
+if operator == '/' and b == 0:
+    print("Error: Division by zero is not allowed")
+    sys.exit(1)
+
+result = ops[operator](a, b)
+print("{} {} {} = {}".format(a, operator, b, result))
