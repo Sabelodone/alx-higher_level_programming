03a2d24321bbc425132dbb93121c242aa6d783e3
Sabelodone <sabelozondo825gmail.com>
2023-08-16 04:23:13 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -8,6 +10,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
