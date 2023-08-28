3931864decb2c1431c7386171108e00103ac67e9
Sabelodone <sabelozondo825gmail.com>
2023-08-28 03:25:13 -0700

    updated

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,8 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         printed_integers = 0
-        for i in range(x):
+        i = 0
+        while i < x:
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 printed_integers += 1
