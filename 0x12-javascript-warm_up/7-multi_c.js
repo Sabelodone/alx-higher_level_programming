2fd54276da620cb951841fcfba42f5f9f460d66c
Sabelodone <sabelozondo825gmail.com>
2023-11-13 06:55:10 -0800

    Write a script that prints x times “C is fun”

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+const process = require('process');
+let numOfTimes = parseInt(process.argv[2]);
+const message1 = 'Missing number of occurrences';
+const message2 = 'C is fun';
+if (isNaN(numOfTimes)) {
+  console.log(message1);
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('C is fun');
+  while (numOfTimes > 0) {
+    console.log(message2);
+    numOfTimes--;
   }
 }
