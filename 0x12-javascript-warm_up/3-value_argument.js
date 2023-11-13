33d37e2cb8efdffb1ab29d22031c38c0097e061b
Sabelodone <sabelozondo825gmail.com>
2023-11-13 03:35:12 -0800

    commit

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2); // Remove the first two elements from process.argv
+let firstArg = args[0];
+
+if (firstArg === undefined) {
+  console.log("No argument");
+} else {
+  console.log(firstArg);
+}
