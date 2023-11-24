eba599f7db443d38c78f96c93fca59bd8b3e3011
Sabelodone <sabelozondo825gmail.com>
2023-11-23 22:15:12 -0800

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""Script displays all values in states table that match given argument
-Takes four arguments:
-    mysql username
-    mysql password
-    database name
-    name to match
-Connects to default host (localhost) and port (3306)
 """
+Lists all values in the states tables of a database where name
+matches the argument
+"""
+import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name = '{}'\
-            ORDER BY states.id ASC""".format(argv[4]))
-    rows = c.fetchall()
-    for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    c.close()
-    db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
