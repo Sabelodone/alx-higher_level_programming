2044b5319582859fb3135998fcc4e81ff7d42bce
Sabelodone <sabelozondo825@gmail.com>
2023-12-05 12:10:40 +0000

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+def fetch_data(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print("Error fetching data: {}".format(e))
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_data(url)
