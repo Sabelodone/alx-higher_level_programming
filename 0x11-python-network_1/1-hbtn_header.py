95c89ac20c468f333b195e211b235f707fc35e49
Sabelodone <sabelozondo825@gmail.com>
2023-12-05 13:00:06 +0000

    script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-"""A script that takes in a URL,
-Sends a request to the URL,
-And displays the value of the X-Request-Id
-variable found in the header ofthe response.
-"""
+"""Displays the X-Request-Id header variable of a request to a given URL.
 
+Usage: ./1-hbtn_header.py <URL>
+"""
 import sys
 import urllib.request
-from urllib.error import URLError
 
-def get_request_id(url):
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            headers = dict(response.headers)
-            request_id = headers.get("X-Request-Id")
-            if request_id:
-                print(f"The X-Request-Id value is: {request_id}")
-            else:
-                print("No X-Request-Id found in the response headers.")
-    except URLError as e:
-        print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-    else:
-        url = sys.argv[1]
-        get_request_id(url)
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
